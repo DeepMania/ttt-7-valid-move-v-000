0cf452612c64fb3584193e72d7dc064c57e4f4b5
Yana Ustsinovich <yustinovich@icloud.com>
2017-10-30 18:15:27 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,13 +7,10 @@ if position_taken? (board, index) && board[index].to_i.between?(0, 8)
 end
 end
 
-def position_taken? (board, index)
-  if
-    board[index] == " " || board[index] == "" || board[index] == nil
-      return false
-  elsif
-    board[index] == "X" || board[index] == "O"
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
       return true
-else return false
-end
+  else 
+    return false
+  end
 end
