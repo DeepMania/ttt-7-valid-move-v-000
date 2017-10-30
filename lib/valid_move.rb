a1a828f505ac05563b7574c41eb71472d7df303c
Yana Ustsinovich <yustinovich@icloud.com>
2017-10-30 18:22:29 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move? (board, index)
 if position_taken? (board, index) == false && board[index].to_i.between?(0, 8) == true
 return true
  else
-    return true
+    return false
 end
 end
 
