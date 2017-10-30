91971c39a19b1c70485671b73b8291f340d1650c
Yana Ustsinovich <yustinovich@icloud.com>
2017-10-30 17:55:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
 def valid_move? (board, index)
-if position_taken? (board, index)
-   board[index].to_i.between?(0, 8) 
+if position_taken? (board, index) && board[index].to_i.between?(0, 8) 
+  return true
  else
     return false
 end
